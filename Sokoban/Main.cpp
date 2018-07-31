dda0b8cb33e3e95a55b8d39c7b28a2dfce3a06c9
KoiKomei <alex.parisella@gmail.com>
2018-07-31 16:44:25 +0200

    First commit

---
+++ Sokoban/Main.cpp
@@ -0,0 +1,16 @@
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_primitives.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	al_init();
+
+	
+
+	return 0;
+	
+
+}
